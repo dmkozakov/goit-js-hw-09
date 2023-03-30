36ccd03a473ffd5104cc727fa09f1ec9a300a991
dmkozakov <79744089+dmkozakov@users.noreply.github.com>
2023-03-30 14:15:04 +0000

    Deploying to gh-pages from @ dmkozakov/goit-js-hw-09@14ec9629fe04efd3dde129ad729b854adbe29ad8 🚀

---
+++ 01-color-switcher.b32c5a3d.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]")};let n=null;function e(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}t.startBtn.addEventListener("click",(function(a){t.startBtn.disabled=!0,t.stopBtn.disabled=!1,e(),n=setInterval(e,1e3)})),t.stopBtn.addEventListener("click",(function(){t.startBtn.disabled=!1,t.stopBtn.disabled=!0,clearInterval(n)})),t.startBtn.disabled=!1,t.stopBtn.disabled=!0;
+//# sourceMappingURL=01-color-switcher.b32c5a3d.js.map
