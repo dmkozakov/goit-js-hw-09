14ec9629fe04efd3dde129ad729b854adbe29ad8
Dmytro Kozakov <dimakozakov1999@gmail.com>
2023-03-30 17:14:06 +0300

    Add task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
+const refs = {
+  startBtn: document.querySelector("[data-start]"),
+  stopBtn: document.querySelector("[data-stop]"),
+};
 
+let timerId = null;
+
+refs.startBtn.addEventListener("click", onStartBtn);
+refs.stopBtn.addEventListener("click", onStopBtn);
+
+refs.startBtn.disabled = false;
+refs.stopBtn.disabled = true;
+
+function onStartBtn(evt) {
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = false;
+
+  changeColor();
+  timerId = setInterval(changeColor, 1000);
+}
+
+function onStopBtn() {
+  refs.startBtn.disabled = false;
+  refs.stopBtn.disabled = true;
+  clearInterval(timerId);
+}
+
+function changeColor() {
+  document.body.style.backgroundColor = `${getRandomHexColor()}`;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
